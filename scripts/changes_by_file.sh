06ba76a8e69d885228dff42cd4012c97f59cfd52
David Li <davidli012345@gmail.com>
2023-03-09 18:38:29 +0000

    feat: update book

--- scripts/changes_by_file.sh
+++ scripts/changes_by_file.sh
@@ -5,11 +5,11 @@ AUTHORS=$(git log --format='%ae' | sort -u)
 
 # Set the list of file extensions to search for
 EXTENSIONS=("md" "ipynb" "sh" "html")
-
+echo "Author,File"
 # Loop through the authors and file extensions and search for changes
 for author in $AUTHORS; do
     for extension in "${EXTENSIONS[@]}"; do
         # echo "Changes made by $author to .$extension files:"
-        git log --author="$author" --oneline --name-only --pretty=format: | grep "\.$extension$" | sort | uniq -c | sort -rn | head -n 10
+        git log --author="$author" --oneline --name-only --pretty=format: | grep "\.$extension$" | sort | uniq -c | sort -rn | head -n 10 | awk -F' ' '{print $1","$2}'
     done
 done
